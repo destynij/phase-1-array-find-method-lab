8a621ba172ae25c320e794c3a2271ff8743a8daa
Destyni <Destyninjohnson20@icloud.com>
2022-03-29 20:04:54 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(record) {
+    function isWinningYear(year) {
+        if (year.result === "W") {
+            return true 
+        } else {
+            return false
+        }
+    }
+   const winningYearObject = record.find(isWinningYear)
+   if (winningYearObject) {
+       return winningYearObject.year
+   }
+}
